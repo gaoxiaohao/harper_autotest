622d789deadcc3446be0ca835e29b0d90323573f
gxh <13166521192@163.com>
2023-03-31 21:26:44 +0800

    again

--- config/driver_config.py
+++ config/driver_config.py
@@ -30,9 +30,8 @@ class DriverConfig:
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         # 实例化浏览器驱动
         driver = webdriver.Chrome(
-            ChromeDriverManager(url="https://registry.npmmirror.com/-/binary/chromedriver",
-                                latest_release_url="https://registry.npmmirror.com/-/binary/chromedriver/LATEST_RELEASE",
-                                cache_valid_range=365).install(),
+            ChromeDriverManager(
+                cache_valid_range=365).install(),
             options=options)
         # 隐形等待时间
         driver.implicitly_wait(3)
